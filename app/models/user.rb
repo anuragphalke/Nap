4c6952b71c284404106de0fed46a5463aedc673f
tuana erol <tuanaerol59@gmail.com>
2024-11-26 14:49:44 +0100

    user model is updated for devise.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,10 @@ class User < ApplicationRecord
 
   # Validations
   validates :username, presence: true, uniqueness: true, format: { with: /\A\w+\z/, message: "must contain only letters, numbers, and underscores without spaces" }
+  validates :email, format: { with: /\A\S+@\S+\.\S+\z/, message: "must type a valid email format." }
+  validates :password, presence: true, length: { minimum: 8 }, format: {
+    with: /\A(?=.*[A-Z])(?=.*[a-z])(?=.*\d).+\z/,
+    message: "must include at least one uppercase letter, one lowercase letter, and one number"
+  }, if: -> { new_record? || !password.nil? }
 end
 # rubocop:enable Layout/LineLength
