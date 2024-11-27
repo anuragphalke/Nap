88e714b7b688234a866adb225078b96bce411083
tuana erol <tuanaerol59@gmail.com>
2024-11-27 10:09:39 +0100

    Routines controller, model and view is fixed.

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -7,8 +7,17 @@ class Routine < ApplicationRecord
   # Validations
   validates :starttime, presence: true, format: { with: /\A([01]?[0-9]|2[0-3]):([0-5][0-9])\z/, message: "must be a valid time in HH:MM format" }
   validates :endtime, presence: true, format: { with: /\A([01]?[0-9]|2[0-3]):([0-5][0-9])\z/, message: "must be a valid time in HH:MM format" }
-
-  # Validates day to be one of the days of the week
   validates :day, presence: true, inclusion: { in: %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday], message: "%<value>s is not a valid day" }
+  validate :endtime_after_starttime
+
+  private
+
+def endtime_after_starttime
+  return if endtime.blank? || starttime.blank?
+
+  if endtime <= starttime
+    errors.add(:endtime, "must be after the start time")
+  end
+end
 end
 # rubocop:enable Layout/LineLength
