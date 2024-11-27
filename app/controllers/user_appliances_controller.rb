efa2ad8b9c0a856f630a17109d9646a57a630e92
Anastasia Polovinkina <apolovika@gmail.com>
2024-11-27 17:10:59 +0100

    not working yet

--- app/controllers/user_appliances_controller.rb
+++ app/controllers/user_appliances_controller.rb
@@ -13,6 +13,7 @@ class UserAppliancesController < ApplicationController
     .group("EXTRACT(HOUR FROM datetime), EXTRACT(DOW FROM datetime)")
     .order("day, hour")
 
+
   end
 
 
@@ -33,15 +34,25 @@ class UserAppliancesController < ApplicationController
 
   def new
     @user_appliance = UserAppliance.new
+    # Get all unique brands from the AllAppliance model
+    @brands = AllAppliance.distinct.pluck(:brand)
+
+    # If a brand is selected, get models for that brand
+    if params[:brand].present?
+      @models = AllAppliance.where(brand: params[:brand]).pluck(:model)
+    else
+      @models = []
+    end
   end
 
   def create
-
+    @models = AllAppliance.where(brand: appliance_params[:brand]).pluck(:model)
     @user_appliance = UserAppliance.new(appliance_params)
     @user_appliance.user_id = current_user.id
 
+
     if @user_appliance.save
-      redirect_to @user_appliance, notice: "appliance was created"
+      redirect_to @user_appliance, notice: "Appliance was successfully added."
     else
       render :new, status: :unprocessable_entity
     end
@@ -50,12 +61,12 @@ class UserAppliancesController < ApplicationController
   def destroy
     @user_appliance = UserAppliance.find(params[:id])
     @user_appliance.destroy
-    redirect_to appliances_path, notice: "appliance deleted"
+    redirect_to user_appliances_path, notice: "appliance deleted"
   end
 
   private
 
   def appliance_params
-    params.require(:user_appliance).permit(:name, :subcategory)
+    params.require(:user_appliance).permit(:all_appliance_id, :brand)
   end
 end
