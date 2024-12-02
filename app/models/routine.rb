0bede59f5c47fc25632dcaaf1fbbdeae36b7dc1c
Anurag Phalke <anurag.phalke@gmail.com>
2024-12-02 12:31:25 +0100

    Recommender and routines fixed, tags added for user_appliance index

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -10,20 +10,11 @@ class Routine < ApplicationRecord
   # validates :starttime, presence: true, format: { with: /\A([01]?[0-9]|2[0-3]):([0-5][0-9])\z/, message: "must be a valid time in HH:MM format" }
   # validates :endtime, presence: true, format: { with: /\A([01]?[0-9]|2[0-3]):([0-5][0-9])\z/, message: "must be a valid time in HH:MM format" }
   validates :day, presence: true, inclusion: { in: [1, 2, 3, 4, 5, 6, 7], message: "%<value>s is not a valid day" }
-  validate :endtime_after_starttime
 
   private
 
   def set_lineage
     return self.update(lineage: self.id) unless self.lineage
   end
-
-def endtime_after_starttime
-  return if endtime.blank? || starttime.blank?
-
-  if endtime <= starttime
-    errors.add(:endtime, "must be after the start time")
-  end
-end
 end
 # rubocop:enable Layout/LineLength
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {registrations: "registrations"}
+  devise_for :users, controllers: { registrations: "registrations" }
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -8,22 +8,20 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :recommendations, only: [:index]
-  resources :articles, only: [:index, :show]
+  resources :articles, only: %i[index show]
 
   get "statistics", to: "pages#statistics", as: :statistics
   # Defines the root path route ("/")
   # root "posts#index"
 
-
-
-resources :user_appliances, only: [:index, :show, :edit, :new, :create, :destroy] do
-    resources :routines, only: [:index, :new, :create]# do
+  resources :user_appliances, only: %i[index show edit new create destroy] do
+    resources :routines, only: %i[index new create] # do
     #   resources :recommendations, only: [:index, :show]
     # end
   end
 
-  resources :routines, only: [:create, :show, :update, :edit, :destroy] do
-    resources :recommendations, only: %i[index]
+  resources :routines, only: %i[create show update edit destroy] do
+    resources :recommendations, only: [:index]
   end
-  resources :prices, only: [:index, :create]
+  resources :prices, only: %i[index create]
 end
