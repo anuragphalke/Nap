f5340ef769a8fb52d2b3f8444820e4fda9713919
Anastasia Polovinkina <apolovika@gmail.com>
2024-12-03 16:32:09 +0100

    login js

--- app/javascript/controllers/login_controller.js
+++ app/javascript/controllers/login_controller.js
@@ -2,19 +2,28 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="login"
 export default class extends Controller {
-  static targets = ["email", "password", "button"];
+  static targets = ["email", "password", "button", "blob"];
+
   connect() {
-
-    this.modelTarget.addEventListener("change", () => this.#enableButton());
-
+    this.passwordTarget.addEventListener("keyup", () => this.#enableButton());
+    this.emailTarget.addEventListener("keyup", () => this.#enableButton());
   }
 
   #enableButton() {
-    if (this.brandTarget.value !== "") {
-      this.buttonTarget.classList.remove("btn-secondary")
-      this.buttonTarget.classList.add("btn-active")
-      console.log('working');
+    if (this.emailTarget.value !== "" && this.passwordTarget.value !== "") {
+      // Activate the button and blob
+      this.buttonTarget.classList.remove("btn-secondary");
+      this.buttonTarget.classList.add("btn-active");
 
+      this.blobTarget.classList.remove("blob-login");
+      this.blobTarget.classList.add("blob-login-active");
+    } else {
+      // Revert to original state
+      this.buttonTarget.classList.remove("btn-active");
+      this.buttonTarget.classList.add("btn-secondary");
+
+      this.blobTarget.classList.remove("blob-login-active");
+      this.blobTarget.classList.add("blob-login");
     }
   }
 }
